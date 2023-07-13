fc8a4492d4663b20baa954bfc26fa499b0b71445
gokudaisensei <87324237+gokudaisensei@users.noreply.github.com>
2023-07-13 10:21:40 +0000

    added all wildcard CORS origins

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.routers import users, auth, institutions
 
 app = FastAPI()
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(institutions.router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/ping")
 def pong():
